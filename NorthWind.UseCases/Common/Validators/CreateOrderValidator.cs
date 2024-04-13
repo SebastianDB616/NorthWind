896397a35cf0450f7276b03d700b17f6f40700d9
Sebastian Dorado <ceceptrabajos123@gmail.com>
2024-04-13 02:49:04 -0700

    Commit de Carpeta Validators + Clases CreateOrderValidators + Validator

---
+++ NorthWind.UseCases/Common/Validators/CreateOrderValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using NorthWind.UseCasesDTOs.CreateOrder;
+using NorthWind.UseCasesPorts.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.Common.Validators
+{
+    public class CreateOrderValidator : AbstractValidator<CreateOrderParams>
+    {
+        public CreateOrderValidator()
+        {
+            RuleFor(c => c.CustomerId).NotEmpty()
+                .WithMessage("Debe proporcionar el identificador del cliente");
+            RuleFor(c => c.ShipAddress).NotEmpty()
+                .WithMessage("Debe proporcionar la dirección de envío");
+            RuleFor(c => c.ShipCity).NotEmpty().MinimumLength(3)
+                .WithMessage("Debe proporcionar mínimo tres caracteres del nombre de la ciudad");
+            RuleFor(c => c.ShipCountry).NotEmpty().MinimumLength(3)
+                .WithMessage("Debe proporcionar mínimo tres caracteres del nombre del país");
+            RuleFor(c => c.OrderDetails).NotEmpty()
+                .Must(d => d != null && d.Any())
+                .WithMessage("Deben especificarse los productos de la orden");
+        }
+    }
+}
