ef580df0d1ed14997a32b249de1787b3bf319bf1
Sebastian Dorado <ceceptrabajos123@gmail.com>
2024-04-13 01:03:05 -0700

    Commit de Carpeta Filters + Clase Filters

---
+++ NorthWind.WebExceptionsPresenter/Filters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthWind.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+    public class Filters
+    {
+        public static void Register(MvcOptions options)
+        {
+            options.Filters.Add(new ApiExceptionFilterAttribute(
+                new Dictionary<Type, IExceptionHandler>
+                {
+                    { typeof(GeneralException), new GeneralExceptionHandler()},
+                    { typeof(ValidationException), new ValidationExceptionHandler() }
+                }
+                ));
+        }
+    }
+}
