bc4f57961439e0f7cde4510ab5bcd1109195d4d8
Sebastian Dorado <ceceptrabajos123@gmail.com>
2024-04-13 00:59:16 -0700

    Commit de Carpeta Ports dentro de UseCases.Common + Interfaces IInputPort y IOutputPort

---
+++ NorthWind.UseCases/Common/Ports/IInputPort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.Common.Ports
+{
+    public interface IInputPort<InteractorRequestType, InteractorResponseType> : IRequest
+    {
+        public InteractorRequestType RequestData { get; }
+        public IOutputPort<InteractorResponseType> OutputPort { get; }
+    }
+}
