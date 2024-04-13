3c60b3922b12d00bf616b64ea35012b90ec1a892
Sebastian Dorado <ceceptrabajos123@gmail.com>
2024-04-13 02:47:27 -0700

    Commit de IoC actualizado

--- NorthWind.IoC/DependencyContainer.cs
+++ NorthWind.IoC/DependencyContainer.cs
@@ -4,10 +4,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NorthWind.Entities.Interfaces;
+using NorthWind.Presenters;
 using NorthWind.Repositories.EFCore.DataContext;
 using NorthWind.Repositories.EFCore.Repositories;
-using NorthWind.UseCases.Common.Behaviors;
+using NorthWind.UseCases.Common.Validators;
 using NorthWind.UseCases.CreateOrder;
+using NorthWind.UseCasesPorts.CreateOrder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,10 +29,10 @@ namespace NorthWind.IoC
             services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            services.AddMediatR(typeof(CreateOrderInteractor));
             services.AddValidatorsFromAssembly(typeof(CreateOrderValidator).Assembly);
-            services.AddTransient(typeof(IPipelineBehavior<,>),
-                typeof(ValidationBehavior<,>));
+            services.AddScoped<ICreateOrderInputPort, CreateOrderInteractor>();
+            services.AddScoped<ICreateOrderOutputPort, CreateOrderPresenter>();
+
             return services;
         }
     }
