c17342cc3b09ba62edc70c679f19008346aba24b
Sebastian Dorado <ceceptrabajos123@gmail.com>
2024-04-13 02:47:07 -0700

    Commit Carpeta Controllers + Clase OrderController

--- NorthWind.Controllers/OrderController.cs
+++ NorthWind.Controllers/OrderController.cs
@@ -3,23 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using NorthWind.Presenters;
 using NorthWind.UseCases.CreateOrder;
 using NorthWind.UseCasesDTOs.CreateOrder;
-    
+using NorthWind.UseCasesPorts.CreateOrder;
+
 namespace NorthWind.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class OrderController
     {
-        readonly IMediator Mediator;
-        public OrderController(IMediator mediator) =>
-            Mediator = mediator;
+        readonly ICreateOrderInputPort InputPort;
+        readonly ICreateOrderOutputPort OutputPort;
+       
+        public OrderController(ICreateOrderInputPort inputPort,
+            ICreateOrderOutputPort outputPort) =>
+            (InputPort, OutputPort) = (inputPort, outputPort);
 
         [HttpPost("create-order")]
         public async Task<string> CreateOrder(
-            CreateOrderParams orderparams)
+            CreateOrderParams orderParams)
         {
-            CreateOrderPresenter Presenter = new CreateOrderPresenter();
-            await Mediator.Send(new CreateOrderInputPort(orderparams, Presenter));
+            await InputPort.Handle(orderParams);
+            var Presenter = OutputPort as CreateOrderPresenter;
             return Presenter.Content;
         }
     }
